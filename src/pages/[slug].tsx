5b05108836ab05bb93005c9b06bb48a8cde109c8
shipmaal <shipmaal@bc.edu>
2023-09-26 15:30:48 -0400

    refixed posts

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -83,7 +83,7 @@ export const getStaticPaths = async () => {
   const slugs = await client.fetch(postSlugsQuery)
 
   return {
-    paths: slugs?.map(({ slug }) => `/post/${slug}`) || [],
+    paths: slugs?.map(({ slug }) => `/${slug}`) || [],
     fallback: 'blocking',
   }
 }
